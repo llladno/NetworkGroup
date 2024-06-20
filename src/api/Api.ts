25b0514705ea905872a639418307593fa5d5f69d
llladno <man30968@gmail.com>
2024-06-20 16:59:49 +0300

    test deploy

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,4 +1,5 @@
 import {Modification} from "../types/types.ts";
+
 class Api {
 
     modifications: Modification[] = []
@@ -8,7 +9,7 @@ class Api {
     }
 
     async fetchData() {
-        this.modifications = await fetch('/data/data.json').then(res => res.json()).then(result => result)
+        this.modifications = await fetch('/data/data.json').then(res => res.json()).then(result => result).catch(err => console.log(err))
     }
 
     getAllModifications(): Promise<Modification[]> {
@@ -35,13 +36,19 @@ class Api {
         })
     }
 
-    getModificationsImg(): Promise<{id: number, img: string}[]> {
+    getModificationsImg(): Promise<{ id: number, img: string }[]> {
         return new Promise((resolve) => {
             if (this.modifications.length === 0) {
-                this.fetchData().then(()=>{
-                    resolve(this.modifications.map((modification) => ({id: modification.id, img: modification.img.preview})))
+                this.fetchData().then(() => {
+                    resolve(this.modifications.map((modification) => ({
+                        id: modification.id,
+                        img: modification.img.preview
+                    })))
                 })
-            } else resolve(this.modifications.map((modification) => ({id: modification.id, img: modification.img.preview})))
+            } else resolve(this.modifications.map((modification) => ({
+                id: modification.id,
+                img: modification.img.preview
+            })))
         })
     }
 }
